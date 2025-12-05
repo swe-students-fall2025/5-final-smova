d0ea8cb9b3fb35b88d91c019279c0dde805be325
vaishnavi-suresh <vs2753@nyu.edu>
2025-12-05 13:54:27 -0500

    create backend structures

---
+++ backend/schema.js
@@ -0,0 +1,51 @@
+const mongoose = require("mongoose");
+
+const userSchema = new mongoose.Schema({
+    fname: String,
+    lname: String, 
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+    },
+    password: String
+})
+
+const movieSchema = new mongoose.Schema({
+    movie_name: String,
+    movie_id:{
+        type: Number,
+        required: true,
+        unique: true
+
+    },
+    movie_description: String, 
+    has_watched: Boolean,
+    rating: Number,
+
+})
+
+const messages = new mongoose.Schema({
+    timestamp: {
+        type: Date,
+        default: Date.now,
+    },
+    contemt: String,
+    role: ["user", "model"],
+    convo_id: {
+        type: Number,
+        required: true,
+        unique: true
+    }
+
+})
+
+const convo = new mongoose.Schema({
+    user_email:String,
+    convo_id: {
+        type: Number,
+        required: true,
+        unique: true
+    },
+    messages: [messages]
+})
